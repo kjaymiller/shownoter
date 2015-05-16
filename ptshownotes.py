080c24744bfa6278baf418fbbc8c2f7bd2a7b0a8
Kevin Miller <kjaymiller@Kevins-MacBook-Pro.local>
2015-05-16 11:00:55 -0700

    new file reads links in chat with "> link (#category)". If no category is given then it will accept the previous category

--- ptshownotes.py
+++ ptshownotes.py
@@ -1,13 +1,25 @@
+notes = {}
+
 def get_notes(file):
     with open(file,'r') as f:
-        new_file = ''
+        key = ''
         for line in f:
-            if line.strip() and not line.startswith('#'):
-                new_line = line.split('-', 1)
-                new_file += ('* [{}]({})\n'.format(new_line[0],new_line[1].strip()))
-            else:
-                new_file += line
-        return(new_file)
-filename = input('Name of File: ')+'.txt'
-with open('submission-{}'.format(filename), 'w') as f:
-    f.write(get_notes(filename))
+            if line.startswith('>'):                
+                if '#' in line:
+                    val = line[1:line.find(' #')].strip()
+                    key = line[line.find('#'):].strip()
+                    print(key, val)
+                    notes[key] = notes.get(key, [val])
+                    if not val in notes[key]:
+                        notes[key].append(val) 
+                else:
+                    val = line[1:line.find(' #')].strip()
+                    notes[key] = notes.get(key, [val])
+                    if not val in notes[key]:
+                        notes[key].append(val)
+
+filename = input('Name of File: ') + '.txt'
+get_notes(filename)
+print(notes)
+                
+        
