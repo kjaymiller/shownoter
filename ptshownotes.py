67486a2ebf257a5d1987330a0f4e240bd01417dd
Kevin Miller <kjaymiller@Kevins-MacBook-Pro.local>
2015-05-23 09:19:20 -0700

    new input format to make loading links and categories easier. Also better recognition of links

--- ptshownotes.py
+++ ptshownotes.py
@@ -5,23 +5,23 @@ from bs4 import BeautifulSoup
 
 def finder(filename):
     with open(filename,'r') as file:
-        return re.findall(r'^>.*', file.read().replace(u'\xa0', u' '), re.M)
-
+        return re.findall(r'^#.*|^\S+\..*',file.read().replace(u'\xa0', u' '),re.M)
 def cats(rlist):
     rdict = defaultdict(list)
     key = 'uncategorized'
     for line in rlist:
-        nline = line[1:].strip().split(' ',1)
-        if not nline[0].startswith('http'):
-             nline[0] = 'http://' + nline[0]
+        nline = line.strip()
+        if nline.startswith('#'):
+            key = nline
+            continue
+        elif not nline.startswith('http'):
+                nline = 'http://' + nline
         try:
-            soup = BeautifulSoup(urllib.request.urlopen(nline[0]))
+            soup = BeautifulSoup(urllib.request.urlopen(nline))
             rtitle = re.sub('[\n\t]','',str(soup.title)[7:-8].strip())
-            if len(nline) == 2:
-                key = nline[1]
-            rdict[key].append((rtitle,nline[0]))
+            rdict[key].append((rtitle,nline))
         except:
-            print(nline[0], 'failed') 
+            print(nline, 'failed') 
     return rdict    
 
 def org(rdict):
@@ -32,6 +32,6 @@ def org(rdict):
             rstr = rstr + '* [{}]({})\n'.format(item[0],item[1])
     return rstr
 
-links = org(cats(finder('raw_pt_ep2.txt')))
+links = org(cats(finder('test.txt')))
 print(links)
 
