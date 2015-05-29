62a5834c772a638b070f3fbcdb945faf70360270
kjaymiller <kjaymiller@gmail.com>
2015-05-29 11:54:31 -0700

    cleaned up code a little, switched back to requests as it handled the encoding better.

--- ptshownotes.py
+++ ptshownotes.py
@@ -1,37 +1,46 @@
 import re
-import urllib.request
+import requests
 from collections import defaultdict
 from bs4 import BeautifulSoup
 
-def finder(filename):
+def find_links(filename):
     with open(filename,'r') as file:
-        return re.findall(r'^#.*|^\S+\..*',file.read().replace(u'\xa0', u' '),re.M)
-def cats(rlist):
+        return re.findall(r'^#.*|^\S+\..*', file.read(), re.M)
+
+def get_links(rlist):
     rdict = defaultdict(list)
     key = 'uncategorized'
+
     for line in rlist:
         nline = line.strip()
+
         if nline.startswith('#'):
             key = nline
             continue
         elif not nline.startswith('http'):
                 nline = 'http://' + nline
+
         try:
-            soup = BeautifulSoup(urllib.request.urlopen(nline))
+            soup = BeautifulSoup(requests.get(nline).content)
             rtitle = re.sub('[\n\t]','',str(soup.title)[7:-8].strip())
             rdict[key].append((rtitle,nline))
         except:
             print(nline, 'failed') 
+
     return rdict    
 
-def org(rdict):
+def create_markdown(rdict):
     rstr = ''
+
     for key in rdict:
         rstr = rstr + '#{}\n'.format(key)
         for item in rdict[key]:
             rstr = rstr + '* [{}]({})\n'.format(item[0],item[1])
+
     return rstr
 
-links = org(cats(finder('test.txt')))
+##############################################################
+
+links = create_markdown(get_links(find_links(input('Enter a Filename: '))))
 print(links)
 
