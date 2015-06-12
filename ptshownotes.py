5df2de93d7ce91ebd328bbf0d21629fcae31ece6
kjaymiller <kjaymiller@gmail.com>
2015-06-11 22:42:57 -0700

    join functionality for dealing with pesky encoding issues.
    Apparently Github has the unicode character \bx7 which doesn't play with python.

--- ptshownotes.py
+++ ptshownotes.py
@@ -3,44 +3,40 @@ import requests
 from collections import defaultdict
 from bs4 import BeautifulSoup
 
-def find_links(filename):
-    with open(filename,'r') as file:
-        return re.findall(r'^#.*|^\S+\..*', file.read(), re.M)
-
-def get_links(rlist):
+def finder(filename):
+    with open(filename,'r', encoding = 'utf8') as file:
+        return re.findall(r'^#.*|^\S+\..*',file.read(),re.M)
+def cats(rlist):
     rdict = defaultdict(list)
     key = 'uncategorized'
-
     for line in rlist:
         nline = line.strip()
-
         if nline.startswith('#'):
             key = nline
             continue
         elif not nline.startswith('http'):
                 nline = 'http://' + nline
-
         try:
-            soup = BeautifulSoup(requests.get(nline).content)
-            rtitle = re.sub('[\n\t]','',str(soup.title)[7:-8].strip())
+            web = requests.get(nline)
+            soup = BeautifulSoup(web.content)
+            try:
+                rtitle = soup.title.string
+            except:
+                rtitle = soup.title.string.encoding('utf8')
+            
+            rtitle = list(rtitle)
+            rtitle = "".join(i for i in rtitle if ord(i)<128)
             rdict[key].append((rtitle,nline))
         except:
             print(nline, 'failed') 
-
     return rdict    
 
-def create_markdown(rdict):
+def org(rdict):
     rstr = ''
-
     for key in rdict:
         rstr = rstr + '#{}\n'.format(key)
         for item in rdict[key]:
             rstr = rstr + '* [{}]({})\n'.format(item[0],item[1])
-
     return rstr
 
-##############################################################
-
-links = create_markdown(get_links(find_links(input('Enter a Filename: '))))
-print(links)
 
