7827128df54dc051267e9a310dc043b656cc428f
kjaymiller <kjaymiller@gmail.com>
2015-06-19 08:13:18 -0700

    Just a little code cleaning
    Made code look better and added a few notes

--- ptshownotes.py
+++ ptshownotes.py
@@ -5,38 +5,45 @@ from bs4 import BeautifulSoup
 
 def finder(filename):
     with open(filename,'r', encoding = 'utf8') as file:
-        return re.findall(r'^#.*|^\S+\..*',file.read(),re.M)
+        return re.findall(r'^#.*|^\S+\..+\b', file.read(),re.M)
+        
+        #Need to make some tests for this regex
+        
 def cats(rlist):
     rdict = defaultdict(list)
     key = 'uncategorized'
+    
     for line in rlist:
         nline = line.strip()
+        
         if nline.startswith('#'):
             key = nline
             continue
+        
         elif not nline.startswith('http'):
                 nline = 'http://' + nline
+                
         try:
             web = requests.get(nline)
             soup = BeautifulSoup(web.content)
-            try:
-                rtitle = soup.title.string
-            except:
-                rtitle = soup.title.string.encoding('utf8')
-            
-            rtitle = list(rtitle)
-            rtitle = "".join(i for i in rtitle if ord(i)<128)
+            rtitle = str().join(c for c in soup.title.text if ord(i)<128)
             rdict[key].append((rtitle,nline))
+            
         except:
-            print(nline, 'failed') 
-    return rdict    
+            print(nline, 'failed')
+    
+    return rdict
 
 def org(rdict):
-    rstr = ''
+    rstr = str()
     for key in rdict:
         rstr = rstr + '#{}\n'.format(key)
+        
         for item in rdict[key]:
             rstr = rstr + '* [{}]({})\n'.format(item[0],item[1])
+    
     return rstr
 
-
+notes = finder('test.txt')
+links = org(cats(notes))
+print(links)
