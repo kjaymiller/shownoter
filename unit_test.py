bca1ac52772b30ce61c190bcb553952c36c1ff0c
kjaymiller <kjaymiller@gmail.com>
2015-09-03 22:49:49 -0700

    re_link does not find detect proper sentences

--- unit_test.py
+++ unit_test.py
@@ -1,8 +1,14 @@
 import pytest
+from link import re_link
 
 def test_link_scrapes_text():
-    from link import re_link 
     links = ('foo.com', 'www.foo.com', 'http://foo.com')
     test_links = map(re_link, links)
 
-    assert (all(test_links))
+    assert all(test_links)
+
+def test_links_doesnt_detect_link():
+    not_links = ('google dot com', 'foo bar', 'This is a serious one. It should not detect sentences')
+    test_links = map(re_link, not_links)
+
+    assert not any(test_links)
