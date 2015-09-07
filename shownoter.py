ac5b45d28d7139b83aa88cbcfdcef34a10c6d234
kjaymiller <kjaymiller@gmail.com>
2015-09-07 15:48:53 -0700

    removed pointless functions from shownoter

--- shownoter.py
+++ shownoter.py
@@ -4,11 +4,3 @@ def re_link(text):
     re_link =  re.compile(r'^#.*|\b\S+\.\S+', re.M)
     result =  re.findall(re_link, text)
     return result
-
-def get_site(url):
-    result = requests.get(url)
-    return result.content
-
-def get_title(site):
-    title = soup.title(site)
-    return title
