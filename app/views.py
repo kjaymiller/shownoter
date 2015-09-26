90ebba1b554dfdb402022510350b47e7c93e48a6
kjaymiller <kjaymiller@gmail.com>
2015-09-26 15:20:51 -0700

    corrections to views.py. reformat

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app
 from collections import namedtuple
 from flask import render_template
-from shownoter import re_link, validate_link, get_title, detect_image
+from shownoter import re_link, validate_link, get_title, detect_image, get_markdown
 from itertools import filterfalse
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,10 +17,12 @@ def index():
 
         images = filter(detect_image, links)
         potential_sites = {(link, validate_link(link)) for link in links if link not in images}
-        sites = {site(link.url, get_title(link.content)) for link in potential_sites if link[1]}
+        sites = {site(link[0], get_title(link[1])) for link in potential_sites if link[1]}
+
         markdown_images = {get_markdown(image) for image in images}
         markdown_sites = {get_markdown(link.url, title=link.title) for link in sites}
         
         markdown = markdown_sites.union(markdown_images)        
+
         return render_template('index.html', form=form, results=markdown)
     return render_template('index.html', form=form)
