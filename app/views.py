a377a879f5bff29f10d2b57f4d14c4e90f3eefd6
kjaymiller <kjaymiller@gmail.com>
2015-09-27 10:47:21 -0700

    added sets to reduce processing time

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from app import app
 from collections import namedtuple
 from flask import render_template
 from shownoter import re_link, validate_link, get_title, detect_image, get_markdown
-from itertools import filterfalse
+from itertools import chain
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -11,18 +11,18 @@ def index():
     form = InputTextForm()
     if form.validate_on_submit():
         text_input = form.chat_text.data
-        links = re_link(text_input)
-        
+        links = re_link(text_input) 
+
+        images = list(filter(detect_image, links))
+
         site = namedtuple('site', ['url', 'title'])
+        potential_sites = ((link, validate_link(link)) for link in links if link not in images)
+        sites = [site(link[0], get_title(link[1])) for link in potential_sites if link[1]]
 
-        images = filter(detect_image, links)
-        potential_sites = {(link, validate_link(link)) for link in links if link not in images}
-        sites = {site(link[0], get_title(link[1])) for link in potential_sites if link[1]}
-
-        markdown_images = {get_markdown(image) for image in images}
-        markdown_sites = {get_markdown(link.url, title=link.title) for link in sites}
+        markdown_images = [get_markdown(image) for image in images]
+        markdown_sites = [get_markdown(link.url, title=link.title) for link in sites]
         
-        markdown = markdown_sites.union(markdown_images)        
+        markdown = list(chain(markdown_sites, markdown_images))        
 
         return render_template('index.html', form=form, results=markdown)
     return render_template('index.html', form=form)
--- shownoter.py
+++ shownoter.py
@@ -4,27 +4,23 @@ from bs4 import BeautifulSoup
 
 def re_link(text):
     re_link =  re.compile(r'\b\S+\.[a-zA-Z]{2,}\S*', re.M)
-    results = []
-    for link in  re.findall(re_link, text):
-        url_start = any([link.startswith('http://'), link.startswith('https://')])
-        results.append('http://' + link if not url_start else link)
-    return results
+    return set(re.findall(re_link, text))
 
 def detect_image(link):
     image_extensions = ['.jpg', '.png', '.gif']
-    match = re.search(r'^.+(?P<extension>\.\S+)$', link)
+    match = re.search(r'^.+(?P<extension>\.\S+)', link)
     return match.group('extension') in image_extensions
 
 def validate_link(link):
     try:
-        request = get(link, timeout='1.5')
+        request = get(link, timeout= 1.5)
     except:
         return False 
     else:
         return request 
 
 def  get_title(link):
-    soup = BeautifulSoup(link.text)
+    soup = BeautifulSoup(link.text, 'html.parser')
     return soup.title.string
 
 def get_markdown(url, title = ''):
