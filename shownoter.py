d6faa2fb4b762483be49c3a48cbc9f06e740eda7
kjaymiller <kjaymiller@gmail.com>
2015-10-17 01:52:27 -0700

    add shownoter.link_detect

--- shownoter.py
+++ shownoter.py
@@ -1,2 +1,8 @@
+import re
+
 def link_detect(text):
-    pass
+    re_link =  re.compile(r'\b\S+\.[a-zA-Z]{2,}\S*', re.M)
+    return re.findall(re_link, text)
+
+def valid_link(text):
+    return '{}{}'.format('' if text.
--- unit_test.py
+++ unit_test.py
@@ -1,11 +1,22 @@
 import shownoter
 import pytest
 
-def test_link_detect_finds_link_in_text():
-    text = '''This is a test
+def test_link_detect_finds_one_link_text():
+    sample_text = '''This is a test
     to see if our regex
     will find link.com
-    and return it'''
-    
-    assert shownoter.link_detect(text) == ['link.com']
-    
+    and return them both'''
+    assert shownoter.link_detect(sample_text) == ['link.com']
+
+def test_link_detect_finds_multiple_links():
+    sample_text = '''This is a test
+    to see if our regex
+    will find link.com
+    and link.net
+    and foo.bar
+    and returns them all'''
+    assert shownoter.link_detect(sample_text) == ['link.com', 'link.net', 'foo.bar']
+
+
+def test_valid_link():
+    assert shownoter.valid_link('link.com') == 'http://link.com'  
