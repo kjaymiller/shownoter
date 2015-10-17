420520e229dfe23ab3fd09ec97d30a2bdc2ab29c
kjaymiller <kjaymiller@gmail.com>
2015-10-17 02:46:13 -0700

    add shownoter.markdown and link

--- shownoter.py
+++ shownoter.py
@@ -2,16 +2,31 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-def link_detect(url):
+def link_detect(site):
     re_link =  re.compile(r'\b\S+\.[a-zA-Z]{2,}\S*', re.M)
-    return re.findall(re_link, url)
+    return re.findall(re_link, site)
 
-def valid_link(url):
+def valid_link(site):
     prefix = ''
-    if not re.search(r'\w{3,5}://', url):
+    if not re.search(r'\w{3,5}://', site):
         prefix += 'http://'
-    return prefix + url
+    return prefix + site
 
-def link_title(url):
-    r = requests.get(url)
+def title(site):
+    r = requests.get(site)
     return BeautifulSoup(r.text, 'html.parser').title.text
+
+def markdown(site, title):
+    return '{}[{}]({})'.format('!' if not title else '', title, site)
+
+def link(site):
+    link_url = valid_link(site)
+    link_title = title(site)
+    link_markdown = markdown(site=link_url, title=link_title)
+    
+    return {
+    'url':link_url,
+    'title':link_title,
+    'markdown':link_markdown
+    }
+
--- unit_test.py
+++ unit_test.py
@@ -30,10 +30,29 @@ def mock_html():
     return '<html><head><title>Test</title></head></html>'
 
 @requests_mock.Mocker(kw='mock')
-def test_link_title(mock_html, **kwargs):
+def test_title(mock_html, **kwargs):
     link = 'http://link.com' 
     html = kwargs['mock'].get(link, text=mock_html)
-    title = shownoter.link_title(link)
+    title = shownoter.title(link)
     assert html.called
     assert title == "Test"
-    
+
+def test_markdown():
+    link = 'link.com'
+    title = 'Test'
+    assert shownoter.markdown(link, title) == '[Test](link.com)'
+
+def test_markdown_if_image():
+    link = 'link.png'
+    title = ''
+    assert shownoter.markdown(link, title) == '![](link.png)'
+
+@requests_mock.Mocker(kw='mock')
+def test_title(mock_html, **kwargs):
+    link = 'http://link.com' 
+    html = kwargs['mock'].get(link, text=mock_html)
+    sample_link = shownoter.link(link)
+    assert html.called
+    assert sample_link['url'] == 'http://link.com'
+    assert sample_link['title'] == 'Test'
+    assert sample_link['markdown'] == '[Test](http://link.com)'
