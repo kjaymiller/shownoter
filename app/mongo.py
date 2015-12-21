9ca5289bc46c4d5dd67c26105578a4d783b0277d
kjaymiller <kjaymiller@gmail.com>
2015-12-20 21:15:27 -0800

    adds docstrings to mongo.py

--- app/mongo.py
+++ app/mongo.py
@@ -5,15 +5,16 @@ conn = pymongo.MongoClient('localhost', 27017)
 db = conn.test
 shownoter = db.shownoter
 
-def save_to_db(description, links):
-    result = shownoter.insert_one({
-        'user':'kjaymiller@gmail.com',
-        'description':description, 
-        'links':links,
-        'title':title})
-
+def create_entry(links):
+    """Adds new link to database"""
+    result = shownoter.insert_one({'links':links})
     return result.inserted_id
 
+def append_to_entry(id, **args):
+    """adds/edits fields to entry in database"""
+    result = shownoter.update_one({'_id':ObjectId(id)}, {'$set': {field:value}}, upsert=True)
+
 def retrieve(id):
+    """fetches results in database"""
     result = shownoter.find_one({'_id':ObjectId(id)})
     return result
