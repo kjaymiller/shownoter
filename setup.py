36d4c4beff60051e3d0391ee9957d6574ef93558
Derek Gustafson <degustaf@gmail.com>
2016-01-04 03:24:51 +0000

    Add TOX for testing and travis-ci integration

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 from distutils.core import setup
+
 setup(name='data_py',
     version='1.0',
     py_modules=['data_py'],
-    )
+    install_requires = [
+        "beautifulsoup4",
+        "Flask",
+        "Flask-WTF",
+        "itsdangerous",
+        "Jinja2",
+        "Markdown",
+        "MarkupSafe",
+        "py",
+        "pymongo",
+        "requests",
+        "six",
+        "Werkzeug",
+        "wheel",
+        "WTForms"
+    ]
+)
