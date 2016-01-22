7c837e32bb514731e53f8a1ee124b6aa8a780909
kjaymiller <kjaymiller@gmail.com>
2016-01-21 19:22:21 -0800

    creates error_handling.py reroutes 404 errors

---
+++ app/error_handling.py
@@ -0,0 +1,14 @@
+"""
+This module handles all of the error routing for shownoter
+author: kjaymiller
+date created: January 21, 2016 
+last modified: January 21, 2016
+"""
+
+from app import app
+from flask import render_template
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
