70cea3feb9b9e89b87971c98b242d2ac28378e64
kjaymiller <kjaymiller@gmail.com>
2016-01-21 20:45:28 -0800

    re-established connection with contributors

--- app/static_files.py
+++ app/static_files.py
@@ -1,9 +1,10 @@
 from app import app
+from app.contributors import contributors
 from flask import render_template
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',contributors=contributors)
 
 @app.route('/report')
 def report():
