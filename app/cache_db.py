d888f7a762bdbf3590dee6ea3faa38389a0db39b
Jay Miller <kjaymiller@gmail.com>
2016-03-05 11:58:05 -0800

    created retrive_from_cache_db renamed retrieve to retrieve_from_db

--- app/cache_db.py
+++ app/cache_db.py
@@ -1,4 +1,6 @@
 import re
+from app.mongo import retrieve_from_db
+from app.mongo import links_coll
 
 
 def remove_url_scheme(url):
@@ -6,7 +8,14 @@ def remove_url_scheme(url):
         returns the domain and extension"""
 
     pattern = re.compile(r'\w{3,5}:\/\/(www\.)?|www\.')
-    new_url = re.sub(pattern,'', url)
+    new_url = re.sub(pattern, '', url)
     return new_url
 
 
+def retreive_from_cache_db(url):
+    """checks links_coll for entries returns url_entry in True or False"""
+    base_url = remove_url_scheme(url)
+    db_entry = retrieve_from_db(value=base_url,
+                                collection=links_coll,
+                                field="domain")
+    return db_entry if db_entry else False
