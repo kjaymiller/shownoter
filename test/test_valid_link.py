02063c749cee31c11d3c97e25516a7f939de4a02
Jay Miller <kjaymiller@gmail.com>
2016-03-06 22:03:47 -0800

    added wrapper around link in html detection (render_link)

--- test/test_valid_link.py
+++ test/test_valid_link.py
@@ -44,16 +44,65 @@ def test_img_detected(good_image):
 
 
 def test_fetch_url_from_html(good_link):
-    assert valid_link.detect_url_from_html(good_link) == 'http://link.com'
+    assert valid_link.detect_url_from_href(good_link) == 'http://link.com'
 
 
-def test_fetch_url_returns_None_if_no_url():
-    assert valid_link.detect_url_from_html(good_image) == None
+def test_fetch_link_from_html_returns_None_if_not_found(good_image):
+    assert valid_link.detect_url_from_href(good_image) == None
 
 
-def test_fetch_title_from_html(good_link):
-    assert valid_link.detect_a_title_in_html(good_link) == 'some title'
+def test_fetch_link_title_from_html(good_link):
+    assert valid_link.detect_title_from_a_tag(good_link) == 'some title'
 
 
-def test_fetch_title_returns_None_if_no_url():
-    assert valid_link.detect_a_title_in_html(good_image) == None
+def test_fetch_link_title_returns_None_if_no_url(good_image):
+    assert valid_link.detect_title_from_a_tag(good_image) == None
+
+
+def test_fetch_image_url_from_html(good_image):
+    assert valid_link.detect_url_from_img(good_image) == 'image.jpg'
+
+
+def test_fetch_image_url_from_html_returns_none_if_not_found(good_link):
+    assert valid_link.detect_url_from_img(good_link) == None
+
+
+def test_fetch_image_title_from_html(good_image):
+    assert valid_link.detect_title_from_img(good_image) == 'some title'
+
+
+def test_fetch_image_title_from_html_returns_None_if_not_found(good_link):
+    assert valid_link.detect_url_from_img(good_link) == None
+
+
+def test_render_link_object_from_html(good_link):
+    """test if dictionary object is returned with title and url from a tag"""
+
+    assert valid_link.render_link_from_html(good_link) == {
+                                                'title': 'some title',
+                                                'url': 'http://link.com'
+                                                }
+
+
+def test_render_link_object_from_html_returns_None_if_none_a(good_image):
+    """tests if function is tested on a non-A_tag"""
+
+    assert valid_link.render_link_from_html(good_image) == None
+
+
+def test_render_link_object_from_html_returns_None_if_no_url():
+    """tests if function is tested on a non-A_tag"""
+
+    missing_url = '<a>There is no Url</a>'
+    assert valid_link.render_link_from_html(missing_url) == None
+
+
+def test_render_link_object_from_html_reutrns_empty_string_if_no_title():
+    """tests in function will complete but return title empty
+    if there is no title"""
+
+    missing_title = '<a href="http://link.com"></a>'
+    assert valid_link.render_link_from_html(missing_title) == {
+                                                    'url': 'http://link.com',
+                                                    'title': ''
+                                                    }
