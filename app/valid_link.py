3ee3174593486ebf5113003374f3202e9b40fe74
Jay Miller <kjaymiller@gmail.com>
2016-03-06 20:13:42 -0800

    added tests to valid_link

--- app/valid_link.py
+++ app/valid_link.py
@@ -1,8 +1,11 @@
 import csv
 import re
 
+
 with open('app/static/tlds.txt', 'r+') as domains:
-    top_level_domains = [domain.lower() for domain in domains.read().splitlines()]
+    top_level_domains = [
+        domain.lower() for domain in domains.read().splitlines()]
+
 
 with open('app/static/uri-schemes-1.csv',
           newline='',
@@ -13,25 +16,36 @@ with open('app/static/uri-schemes-1.csv',
         uris.append(row['URI Scheme'].lower())
 
 
-def extract_from_html(url):
-    """function takes a url and title from html and converts to dict"""
+def detect_a_in_html(a_tag):
+    """checks tag for a record"""
+    if re.search(r'<a.+</a>', a_tag):
+        return True
 
-    re_html = re.compile(r"""<a\ (\w+=["'].["']\ )*
-                        href=["'](?P<url>.+)["']
-                         (\ \w+=["'].["']\ {0,1})*>
-                         (?P<title>.+)
-                         </a>""", re.VERBOSE)
-
-    result = re.match(re_html, url)
-    return result.groupdict()
+    else:
+        return False
 
 
-def extract_from_markdown(url):
-    """function takes title and url from markdown and converts to dict"""
+def detect_img_in_html(img_tag):
+    if re.search(r'<img.+>', img_tag):
+        return True
 
-    re_mkdown = re.complile(r"""(?P<image_flag>!)*
-                            [(?P<title>.*)]
-                            \((<?Purl>.+)\)""", re.VERBOSE)
+    else:
+        return False
 
-    result = re.match(re_mkdown, url)
-    return result.groupdict()
+
+def detect_url_from_html(a_tag):
+    if detect_a_in_html(a_tag):
+        url = re.search(r"""href=["'](?P<url>\S+)["']""", a_tag)
+        return url.group('url')
+
+    else:
+        return None
+
+
+def detect_a_title_in_html(a_tag):
+    if detect_a_in_html(a_tag):
+        title = re.search(r'<a.+>(?P<title>.+)</a>', a_tag)
+        return title.group('title')
+
+    else:
+        return None
