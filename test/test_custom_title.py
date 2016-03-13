46ba4ee99da469ca295eeaa63aa2c3c9585c8d03
kjaymiller <kjaymiller@gmail.com>
2016-03-12 17:29:53 -0800

    adjustment to regex on custom link to reject markdown list param

--- test/test_custom_title.py
+++ test/test_custom_title.py
@@ -12,7 +12,7 @@ def detected_items():
 
 
 def test_detect_custom_link_finds_url(detected_items):
-    assert custom_title.detect_link('Some Value - link.com') == detected_items
+    assert custom_title.detect_link('Some Value - link.com')['url'] == 'link.com'
 
 
 def test_detect_custom_link_returns_None_if_no_space_dash():
@@ -33,3 +33,12 @@ def test_detect_custom_title_finds_colon(detected_items):
 
 def test_detect_custom_title_finds_colon_left_space(detected_items):
     assert custom_title.detect_link('Some Value: link.com') == detected_items
+
+
+def test_detect_custom_title_doesnt_hang_on_starting_mark():
+    test_subjects = ['*', '-']
+
+    for test_subject in test_subjects:
+        test_entry = '{} some value - link.com'.format(test_subject)
+        assert custom_title.detect_link(test_entry) == {'title': 'some value',
+                                                        'url': 'link.com'}
